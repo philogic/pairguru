e6d0a50478e50e9aca809fc661b284ad123cc6d8
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-02-13 04:49:09 +0100

    S_5 Annotate comment model and related files.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  movie_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
   belongs_to :movie
   belongs_to :user
